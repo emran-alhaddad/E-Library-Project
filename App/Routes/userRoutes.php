39afb7cfa686e4aad8e1e81c9a3f6881bae9e370
emranco <emranhadad770774255770774255@gmail.com>
2022-03-28 09:30:20 +0300

    add routing

---
+++ App/Routes/userRoutes.php
@@ -0,0 +1,26 @@
+<?php
+require_once("App/System/System.php");
+require_once(System::SYSTEM . "Express.php");
+require_once(System::CONTROLLER . "controller.php");
+
+
+class userRoutes
+{
+
+    public static function show($url = "")
+    {
+        Express::get($url . '/show', function () {
+            $object = new Express::$controller;
+            call_user_func_array([$object, Express::$method], Express::$parameters);
+        });
+
+        Express::get('/add', function () {
+        });
+
+        Express::get('/eidt', function () {
+        });
+
+        Express::get('/delete', function () {
+        });
+    }
+}
